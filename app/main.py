97ba0c81f3d9836f72055bb44881f42ef3316c4a
bycs <Cartoon2008!>
2021-09-14 16:10:22 +0300

    update: add async

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ import datetime
 import http
 import os
 
-from fastapi import FastAPI, status
+import aioredis
 
-import redis
+from fastapi import FastAPI, status
 
 import tldextract
 
@@ -18,11 +18,14 @@ def url_parsing(url: str) -> str:
 
 
 app = FastAPI()
-redis_client = redis.Redis(
-    host=os.getenv("REDIS_HOST"),
+
+redis_client = aioredis.from_url(
+    f'redis://{os.getenv("REDIS_HOST")}',
     port=os.getenv("REDIS_PORT"),
     db=os.getenv("REDIS_DB"),
     password=os.getenv("REDIS_PASSWORD"),
+    encoding="utf-8",
+    decode_responses=True,
 )
 
 
@@ -31,12 +34,12 @@ redis_client = redis.Redis(
     response_model=ResponseStatusSchema,
     status_code=status.HTTP_200_OK,
 )
-def visited_links_post(links: dict) -> dict:
+async def visited_links_post(links: dict) -> dict:
     datetime_now = datetime.datetime.utcnow().strftime("%y%m%d%H%M")
     links = links["links"]
     for link in links:
-        redis_client.zadd(name="links", mapping={link: datetime_now})
-        redis_client.close()
+        await redis_client.zadd(name="links", mapping={link: datetime_now})
+        await redis_client.close()
     status_detail = http.HTTPStatus.OK.phrase
     return {"status": status_detail}
 
@@ -46,11 +49,11 @@ def visited_links_post(links: dict) -> dict:
     response_model=VisitedDomainsSchema,
     status_code=status.HTTP_200_OK,
 )
-def visited_domains_get(datetime_from: int, datetime_from_to: int) -> dict:
-    domains = redis_client.zrangebyscore(
+async def visited_domains_get(datetime_from: int, datetime_from_to: int) -> dict:
+    domains = await redis_client.zrangebyscore(
         name="links", min=datetime_from, max=datetime_from_to
     )
-    domains = (x.decode("UTF-8") for x in domains)
+    await redis_client.close()
     domains = (url_parsing(x) for x in domains)
     status_detail = http.HTTPStatus.OK.phrase
     return {"domains": list(set(domains)), "status": status_detail}
