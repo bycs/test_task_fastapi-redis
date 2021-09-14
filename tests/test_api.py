b5ebad2c84afb71ace99194113dc74eb146b2a1d
Stas Cartoon <bycs@mail.ru>
2021-09-14 23:43:46 +0300

    update: test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,18 @@
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-from ..app.main import app
+app = FastAPI()
+
+
+@app.get("/")
+async def read_main():
+    return {"msg": "Hello World"}
+
 
 client = TestClient(app)
 
 
-def test_ping():
-    response = client.get("/ping")
+def test_read_main():
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"ping": "pong!"}
+    assert response.json() == {"msg": "Hello World"}
