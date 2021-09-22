2227e810b4fa4c14c170d745ba955ec0570909f9
Stas Cartoon <bycs@mail.ru>
2021-09-23 01:15:11 +0300

    update: working with exceptions

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,26 @@
-from pydantic import BaseModel
+from pydantic import AnyUrl, BaseModel
 
 
 class ResponseStatusSchema(BaseModel):
+    """Схема pydantic для проверки ответа от visited_links_post"""
+
     status: str
+
+
+class RequestsVisitedLinksSchema(BaseModel):
+    """Схема pydantic для проверки запроса для visited_domains_get"""
+
+    links: list[AnyUrl]
 
 
 class VisitedDomainsSchema(BaseModel):
+    """Схема pydantic для проверки ответа от visited_domains_get"""
+
     domains: list[str]
     status: str
+
+
+class UrlSchema(BaseModel):
+    """Схема pydantic для проверки url"""
+
+    url: AnyUrl
